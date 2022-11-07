c9ec178d6fec705ac915207b44e088828aa81d32
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-07 11:26:14 +0500

    Setup Nodejs Server for Backend

--- server/index.js
+++ server/index.js
@@ -3,13 +3,23 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json({ limit: "1mb", extended: true }));
 
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send({ status: 200, message: "Nice work so far" });
+  res.send("App is up and running");
 });
 
-app.listen(PORT, () => {
-  console.log("App listening on ", PORT);
-});
+mongoose
+  .connect(
+    "mongodb+srv://zahidali:Hackersonly123@cluster0.djdawwu.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("App listening on ", PORT);
+    });
+  })
+  .catch((error) => console.log(error));
