bd672937ce8808123f300ec32f61a7b38c53dfa9
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-07 14:22:07 +0500

    Setting up authcontroller

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,14 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "../model/User";
+
 export const singUp = async (req, res) => {
   const { name, email, password } = req.body;
+  const userExists = await User.findById({ email });
+
+  if (userExists) {
+    return res.json({ message: "User already exists" });
+  }
 
   try {
   } catch (error) {}
