d8d7fc04ad893b833634212b00f277c95e91b73e
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-07 13:20:56 +0500

    Setting up reducers

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,9 @@
-const authReducer = () => {};
+const authReducer = (state = [], action) => {
+  switch (action.type) {
+    case "Auth":
+      localStorage.setItem("profile", JSON.stringify({ ...action.data }));
+      console.log(JSON.stringify({ ...action.data }));
+  }
+};
 
 export default authReducer;
