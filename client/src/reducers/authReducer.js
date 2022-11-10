98c3f54e9eb4fcc98e6cc6a9063f5cc050afa7f2
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-11-10 14:35:05 +0500

    working with auth

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,7 +2,6 @@ const authReducer = (state = [], action) => {
   switch (action.type) {
     case "Auth":
       localStorage.setItem("profile", JSON.stringify({ ...action.data }));
-      console.log(JSON.stringify({ ...state, authData: action.data }));
       return { ...state, authData: action.data };
     default:
       return state;
